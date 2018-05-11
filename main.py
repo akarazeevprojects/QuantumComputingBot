d07bb34f078b81ef913a29307633439003460968
Anton Karazeev <anton.karazeev@gmail.com>
2018-05-11 12:17:52 +0300

    Add files

---
+++ main.py
@@ -0,0 +1,99 @@
+from telegram.ext import Updater, CommandHandler, Job, MessageHandler, Filters, CallbackQueryHandler
+from qiskit.backends import discover_local_backends, discover_remote_backends
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+import telegram
+import logging
+import typing
+import sys
+import json
+import re
+import os
+from IBMQuantumExperience import IBMQuantumExperience
+
+from utils import make_plot, myThread
+
+sys.path.append("res/")
+import Qconfig
+
+qx_config = {
+    "APItoken": Qconfig.APItoken,
+    "url": Qconfig.config['url']
+}
+
+api = IBMQuantumExperience(token=qx_config['APItoken'], config={'url': qx_config['url']})
+counter = 0
+
+info_text = []
+info_text.append('You can control me by sending these commands:')
+info_text.append('')
+info_text.append('/ibmqx4 - statistics for ibmqx4 quantum processor')
+info_text.append('/ibmqx5 - statistics for ibmqx5 quantum processor')
+info_text = '\n'.join(info_text)
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - \
+                            %(message)s', level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+def get_token():
+    path = os.path.join('res', 'token.json')
+    with open(path) as jsn:
+        data = json.load(jsn)
+    return data['token']
+
+
+def choose_backend(bot, update):
+    global counter
+    counter += 1
+
+    backends = ['ibmqx4', 'ibmqx5']
+    backend = update.message.text[1:]
+
+    if backend in backends:
+        filename = '{}.png'.format(backend)
+        make_plot(backend, filename)
+
+        user_id = update.message.chat_id
+        bot.send_photo(chat_id=user_id, photo=open(filename, 'rb'))
+    else:
+        update.message.reply_text(info_text)
+
+
+def info(bot, update):
+    update.message.reply_text(counter)
+
+
+def help(bot, update):
+    update.message.reply_text(info_text)
+
+
+def main():
+    updater = Updater(get_token())
+
+    bot = telegram.Bot(token=get_token())
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler('help', help))
+    dp.add_handler(CommandHandler('info', info))
+    dp.add_handler(CommandHandler('start', help))
+    dp.add_handler(MessageHandler(Filters.text, help))
+    dp.add_handler(MessageHandler(Filters.command, choose_backend))
+
+    # Start dumper.
+    run_event = threading.Event()
+    run_event.set()
+    # Create new threads.
+    delay = 60  # Seconds.
+    thread = myThread(delay)
+    thread.start()
+    ########
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,86 @@
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+import threading
+import pickle
+import time
+from datetime import date, timedelta
+from datetime import datetime as dt
+
+
+class myThread(threading.Thread):
+    def __init__(self, delay, run_event):
+        threading.Thread.__init__(self)
+        self.delay = delay
+        self.run_event = run_event
+
+    def run(self):
+        dumper(self.delay, self.run_event)
+
+
+def dumper(delay):
+    if os.path.exists('data.pkl') is False:
+        data = list()
+        with open('data.pkl', 'wb') as f:
+                pickle.dump(data, f)
+
+    while run_event.is_set():
+        # Load.
+        with open('data.pkl', 'rb') as f:
+            data = pickle.load(f)
+
+        # Append.
+        remote_backends = discover_remote_backends(api)
+        device_status = [api.backend_status(backend) for backend in remote_backends]
+
+        data.append((time.time(), device_status))
+
+        # Store.
+        with open('data.pkl', 'wb') as f:
+            pickle.dump(data, f)
+
+        # Sleep.
+        time.sleep(delay)
+
+
+def make_plot(backend, filename):
+    with open('data.pkl', 'rb') as f:
+        data = pickle.load(f)
+
+    times = sorted([x[0] for x in data])
+    pending_jobs = [[y for y in x[1] if y['backend'] == backend][0]['pending_jobs']
+                    for x in sorted(data, key=lambda x: x[0])]
+
+    d1 = dt.fromtimestamp(times[0]).date()
+    d2 = dt.fromtimestamp(times[-1]).date()
+    # Timedelta.
+    delta = d2 - d1
+    days = list()
+    for i in range(delta.days + 1):
+        tmp = d1 + timedelta(days=i)
+        tmp_ts = float(tmp.strftime('%s'))
+        if tmp_ts >= times[0] and tmp_ts <= times[-1]:
+            days.append(tmp_ts)
+
+    plt.figure(figsize=(11, 5))
+    plt.plot(times, pending_jobs, color='blue')
+    plt.grid(color='b', linestyle='--', linewidth=1, alpha=0.3)
+
+    # New xticks.
+    locs, labels = plt.xticks()
+    new_ticks = [dt.fromtimestamp(x).strftime('%H:%M') for x in locs]
+    plt.xticks(locs[1:-1], new_ticks[1:-1], rotation=30, fontsize=15)
+    plt.yticks(fontsize=15)
+
+    # Vertical lines.
+    for day in days:
+        plt.axvline(x=day, color='k', linestyle='-.')
+
+    # Captions.
+    plt.title('{} - Local time of bot: {}'.format(backend,
+              dt.fromtimestamp(time.time()).strftime('%Y, %b %d, %H:%M')),
+              fontsize=15)
+    plt.xlabel('Time', fontsize=15)
+    plt.ylabel('Pending jobs', fontsize=15)
+    plt.savefig(filename, bbox_inches='tight')
